9c561de57d0b41c4f267dfd43b33d629b4cf5828
Sean Nekouei <sean.nekouei@gmail.com>
2022-12-19 17:30:45 -0700

    commit tree cbc32cb4149fac648cf21bae38060a764d9f5a6f

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+const element = document.createElement("div");
+document.body.append(element)
+const newHeader = document.createElement("h1")
+document.head.append(newHeader)
